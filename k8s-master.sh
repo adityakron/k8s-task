266296feaac2d137a69db384e40c6db5a653d8ee
aditya kolte <54710559+adityakron@users.noreply.github.com>
2021-04-09 12:24:25 +0530

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Removing previously installed docker 
-yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y > /dev/null
+swapoff -a
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y > /dev/null 2>&1
 
-yum install -y yum-utils > /dev/null
+yum install -y yum-utils > /dev/null 2>&1
 yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y 
 
@@ -46,11 +47,54 @@ echo "   "
 echo "               Creating cluster!!!!!Wait for few minutes"
 echo "   "
 echo "########################################################################"
+echo "
+#!/bin/bash
 
+#Removing previously installed docker 
+yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine -y > /dev/null
+swapoff -a
+yum install -y yum-utils > /dev/null
+yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io -y 
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+sudo systemctl enable --now kubelet
+
+" > node_join.sh
 
 kubeadm init | tail -2 >> node_join.sh
 
-
+if [ $? -eq 0 ] ; then 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -59,8 +103,15 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://docs.projectcalico.org/v3.11/manifests/calico.yaml
 
 sleep 10
-
-kubectl get nodes
-
-
-
+echo " "
+kubectl get nodes -o wide
+echo " "
+clear
+echo "############################################################################"
+echo " "
+echo " Copy node_join.sh script to worker node. And excecute to join cluster "
+echo " "
+echo " #############################################################################"
+else
+echo " "
+fi
